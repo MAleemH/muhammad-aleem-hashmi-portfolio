9ae457c6bb5e457a32bf5652b0727ab47caadcf0
Muhammad Aleem Hashmi <aleemhashmi4321@gmail.com>
2024-09-20 11:30:19 +0500

    Imported all files in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home';
+import About from './components/about';
+import Projects from './components/projects';
+import Contact from './components/contact';
+import NotMatch from './components/notMatch';
+import Layout from './components/layout';
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>My Portfolio</h1>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
   );
 }
 
