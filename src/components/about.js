1e59e138134a79569b098e4403cb03aca4e3d4fb
Muhammad Aleem Hashmi <aleemhashmi4321@gmail.com>
2024-09-23 10:12:55 +0500

    Updated about page top section

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,20 @@
 const About = () => (
   <div className="main">
-    <h2>About</h2>
+    <div className="container hero-section">
+      <div className="row justify-content-center align-items-center h-100">
+        <div className="col-12">
+          <div className="hero-title text-center">About</div>
+          <p className="text-center">
+            Here you will find more information about me, what I do, and my
+            current skills mostly in terms of programming and technology
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div className="container">
+      lorem ipsum
+    </div>
   </div>
 );
 
