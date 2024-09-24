98ff742106f0afd8704698d97e8abfc3edb75abd
Muhammad Aleem Hashmi <aleemhashmi4321@gmail.com>
2024-09-24 11:20:43 +0500

    Added basic structure for projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,15 @@
 const Projects = () => (
   <div className="main">
-    <h2>Projects</h2>
+    <div className="container hero-section">
+      <div className="row justify-content-center align-items-center h-100">
+        <div className="col-12">
+          <div className="hero-title text-center">Projects</div>
+          <p className="text-center">
+            Here you will find more information about my projects
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
