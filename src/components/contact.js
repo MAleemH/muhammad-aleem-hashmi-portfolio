89f178c151246fe7e5096dea5bb79ebff2fe1dc5
Muhammad Aleem Hashmi <aleemhashmi4321@gmail.com>
2024-09-26 11:33:49 +0500

    Added dynamic text based on screen size

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,21 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
 const Contact = () => (
   <div className="main">
-    <h2>Contact</h2>
+    <div className="container hero-section">
+      <div className="row justify-content-center align-items-center h-100">
+        <div className="col-12">
+          <div className="hero-title text-center">Contact</div>
+          <p className="text-center d-lg-block d-md-block d-none">
+            If you are interested in working with me, contact me below
+          </p>
+          <p className="text-center d-lg-none d-md-none d-block">
+            If you have an application you are interested in developing, a feature
+            that you need built or a project that needs coding. I&apos;d love to help with it
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
